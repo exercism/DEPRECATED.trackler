f919c11de1e036896cc3b70428c6de03194f7a62
Geoff Hubbard <ghub@instihost.com>
2016-11-20 21:26:13 +0000

    Rename misleadingly named test method.

--- test/trackler/problem_test.rb
+++ test/trackler/problem_test.rb
@@ -37,8 +37,7 @@ class ProblemTest < Minitest::Test
     assert_equal "## Source\n\nThe mango. [http://example.com](http://example.com)", problem.source_markdown
   end
 
-  def test_source_markdown_no_source
-    # in deprecated location
+  def test_source_markdown_in_depricated_location
     problem = Trackler::Problem.new('banana', FIXTURE_PATH)
     assert_equal "## Source\n\n[http://example.com](http://example.com)", problem.source_markdown
   end
