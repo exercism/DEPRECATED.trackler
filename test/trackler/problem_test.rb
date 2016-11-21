11f582d467a6e0f6ba1426ee7f6973bef087db06
Geoff Hubbard <ghub@instihost.com>
2016-11-21 07:44:09 +0000

    Only test one thing per test case
    Fixes Rubocop warnings

--- test/trackler/problem_test.rb
+++ test/trackler/problem_test.rb
@@ -4,16 +4,51 @@ require 'trackler/problem'
 class ProblemTest < Minitest::Test
   def test_existing_problem
     problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
-
     assert problem.exists?, "hello world files not found in the metadata dir"
-    assert_equal 'hello-world', problem.slug
-    assert_equal 'Oh, hello.', problem.blurb
-    assert_equal "* hello\n* hello again\n", problem.description
-    assert_equal 'https://github.com/exercism/x-common/blob/master/exercises/hello-world/metadata.yml', problem.yaml_url
-    assert_equal 'https://github.com/exercism/x-common/blob/master/exercises/hello-world/description.md', problem.md_url
+  end
 
+  def test_slug
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
+    assert_equal 'hello-world', problem.slug
+  end
+
+  def test_name
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
+    assert_equal 'Hello World', problem.name
+  end
+
+  def test_blurb
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
+    assert_equal 'Oh, hello.', problem.blurb
+  end
+
+  def test_description
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
+    assert_equal "* hello\n* hello again\n", problem.description
+  end
+
+  def test_yaml_url
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
+    assert_equal 'https://github.com/exercism/x-common/blob/master/exercises/hello-world/metadata.yml', problem.yaml_url
+  end
+
+  def test_md_url
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
+    assert_equal 'https://github.com/exercism/x-common/blob/master/exercises/hello-world/description.md', problem.md_url
+  end
+
+  def test_source_markdown
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
     assert_equal "## Source\n\nThe internet. [http://example.com](http://example.com)", problem.source_markdown
+  end
+
+  def test_source
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
     assert_equal 'The internet.', problem.source
+  end
+
+  def test_source_url
+    problem = Trackler::Problem.new('hello-world', FIXTURE_PATH)
     assert_equal 'http://example.com', problem.source_url
   end
 
