243dd7e77b9c5b18625bc80239c0c62204e0a028
Geoff Hubbard <ghub@instihost.com>
2016-11-26 00:05:23 +0000

    Move file existence checks to a more relevant place.
    The the *_path method should not be concerned about whether the file
    exists, it just needs to know where to look for it.

--- lib/trackler/problem.rb
+++ lib/trackler/problem.rb
@@ -18,7 +18,7 @@ module Trackler
     end
 
     def description
-      @description ||= File.read(common_metadata_path(description_path)) if !!description_path
+      @description ||= File.read(common_metadata_path(description_path)) if File.exists?(common_metadata_path(description_path))
     end
 
     def source_markdown
@@ -52,7 +52,7 @@ module Trackler
     end
 
     def canonical_data_url
-      repo_url(canonical_data_path)
+      repo_url(canonical_data_path) if File.exists?(common_metadata_path(canonical_data_path))
     end
 
     def metadata_url
@@ -60,35 +60,29 @@ module Trackler
     end
 
     def blurb
-      metadata['blurb'].to_s.strip
+      metadata.nil? ? '' :  metadata['blurb'].to_s.strip
     end
 
     def source
-      metadata['source'].to_s.strip
+      metadata.nil? ? '' : metadata['source'].to_s.strip
     end
 
     def source_url
-      metadata['source_url'].to_s.strip
+      metadata.nil? ? '' : metadata['source_url'].to_s.strip
     end
 
     private
 
     def canonical_data_path
-      [
-        "exercises/%s/canonical-data.json" % slug,
-      ].find { |filename| File.exist?(common_metadata_path(filename)) }
+      "exercises/%s/canonical-data.json" % slug
     end
 
     def metadata_path
-      [
-        "exercises/%s/metadata.yml" % slug,
-      ].find { |filename| File.exist?(common_metadata_path(filename)) }
+      "exercises/%s/metadata.yml" % slug
     end
 
     def description_path
-      [
-        "exercises/%s/description.md" % slug,
-      ].find { |filename| File.exist?(common_metadata_path(filename)) }
+      "exercises/%s/description.md" % slug
     end
 
     def repo_url(path)
@@ -96,7 +90,9 @@ module Trackler
     end
 
     def metadata
-      @metadata ||= YAML.load(File.read(common_metadata_path(metadata_path))) if !!metadata_path
+      @metadata ||= if File.exists?(common_metadata_path(metadata_path))
+                      YAML.load(File.read(common_metadata_path(metadata_path)))
+                    end
     end
 
     def common_metadata_path(path)
