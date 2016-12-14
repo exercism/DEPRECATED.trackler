59fb40008ab8c398ee00d4001c5d6c7a64ea8409
Nick Borromeo <ngborromeo@gmail.com>
2016-12-14 03:03:28 -0800

    Make tests pass after adding new config and new fixture

--- test/trackler_test.rb
+++ test/trackler_test.rb
@@ -4,7 +4,7 @@ require_relative '../lib/trackler'
 class TracklerTest < Minitest::Test
   def test_todos
     Trackler.use_fixture_data
-    slugs = %w(banana cherry imbe mango)
+    slugs = %w(banana cherry imbe mango track-and-field)
     assert_equal slugs, Trackler.todos["fake"].map(&:slug)
   end
 end
