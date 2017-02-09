76d719d14c1691306536cbe1654b838a104dd182
Ian Whitney <whit0694@umn.edu>
2017-02-08 18:31:17 -0600

    Test final pieces of Trackler behavior
    As mentioned in #22
    
    ```
    Trackler.problems
    
    Current behavior: Returns a Problems collection that contains all
    problems in the common directory
    
    Proposed behavior: Unchanged
    
    Would it contain track-specific problems? No.
    ```
    
    and
    
    ```
    Trackler.implementations
    
    Current behavior: Returns a Hash. The key is a problem slug, the values
    is an array of all tracks that implement that problem.
    
    Proposed behavior: Same structure, but if a track implements a
    track-only problem, it would be contained in this Hash. So, in the
    example we would now have
    
    {..."rust-only" => [<rust implementation object>]...}
    
    Trackler.implementations['rust-only'] would return a an array with a
    single implementation object.a
    ```
    
    and
    
    ```
    Trackler.todos
    
    Current behavior: Returns a Hash. The key is a track, the value is an
    array of common problems that the track does not implement.
    
    Proposed behavior: Unchanged
    
    Would it contain track-specific problems? No.
    
    Trackler.todos['rust-only'] would return nil.
    ```
    
    These tests exercise these behaviors

--- test/trackler_test.rb
+++ test/trackler_test.rb
@@ -7,4 +7,22 @@ class TracklerTest < Minitest::Test
     slugs = %w(banana cherry imbe mango)
     assert_equal slugs, Trackler.todos["fake"].map(&:slug)
   end
+
+  def test_problems_does_not_contain_track_specific_problems
+    Trackler.use_fixture_data
+
+    assert_nil Trackler.problems.detect { |p| p.slug == "snowflake-only" }
+  end
+
+  def test_problems_does_not_contain_track_specific_problems
+    Trackler.use_fixture_data
+
+    refute_nil Trackler.implementations['snowflake-only']
+  end
+
+  def test_todos_does_not_contain_track_specific_problems
+    Trackler.use_fixture_data
+
+    assert_empty Trackler.todos['snowflake-only']
+  end
 end
