a0fa16fa0a52c4f35f12ea2de9c79bdb835ff29f
Geoff Hubbard <ghub@instihost.com>
2017-04-21 20:51:26 +0100

    Make track attr_reader private

--- lib/trackler/implementation.rb
+++ lib/trackler/implementation.rb
@@ -13,7 +13,6 @@ module Trackler
     extend Forwardable
     def_delegators :@problem, :name, :blurb, :description, :source_markdown, :slug, :source, :metadata, :root, :active?, :deprecated?, :source_url, :description_url, :canonical_data_url, :metadata_url
 
-    attr_reader :track
     def initialize(track, problem)
       @track = track
       @problem = problem
@@ -70,6 +69,8 @@ module Trackler
     end
 
     private
+
+    attr_reader :track
 
     def regexes_to_ignore
       (IGNORE_PATTERNS + [track.ignore_pattern]).map do |pattern|
