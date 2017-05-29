eb17beb7291dc01d13ec991700063af98a4c4892
Katrina Owen <kytrinyx@github.com>
2017-05-29 14:38:08 -0600

    Add Specification to replace Problem
    This does not remove the 'Problem' class, it inherits from it and replaces the tests
    so that we can change all of the downstream dependencies without breaking anything.
    
    See #40.

---
+++ lib/trackler/specification.rb
@@ -0,0 +1,7 @@
+require 'trackler/problem'
+
+module Trackler
+  # Specification is a language-independent definition of a problem to solve.
+  class Specification < Trackler::Problem
+  end
+end
---
+++ lib/trackler/specifications.rb
@@ -0,0 +1,7 @@
+require 'trackler/problems'
+
+module Trackler
+  # Specifications is the collection of problems that we have metadata for.
+  class Specifications < Trackler::Problems
+  end
+end
