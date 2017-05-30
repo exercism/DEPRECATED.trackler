dc9403178eb13bc2dd08bc42966752b871488178
Geoff Hubbard <ghub@instihost.com>
2017-05-30 20:26:47 +0100

    Add unit tests for track hints

--- test/trackler/track_test.rb
+++ test/trackler/track_test.rb
@@ -193,4 +193,22 @@ class TrackTest < Minitest::Test
       assert_equal 'example', track.ignore_pattern
     end
   end
+
+  def test_track_hints
+    track = Trackler::Track.new('animal', FIXTURE_PATH)
+    expected = "This is the content of the track hints file\n"
+    assert_equal expected, track.hints
+  end
+
+  def test_track_hints_deprecated_location
+    track = Trackler::Track.new('fruit', FIXTURE_PATH)
+    expected = "The SETUP.md file is deprecated, and exercises/TRACK_HINTS.md should be used.\n"
+    assert_equal expected, track.hints
+  end
+
+  def test_track_hints_not_present
+    track = Trackler::Track.new('shoes', FIXTURE_PATH)
+    expected = ""
+    assert_equal expected, track.hints
+  end
 end
