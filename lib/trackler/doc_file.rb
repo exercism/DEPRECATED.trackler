00ee138705aa2ade29863d9cecd7d6ceb8a0aeb9
Katrina Owen <kytrinyx@github.com>
2017-07-01 09:54:00 -0600

    Namespace the docfiles
    I should have caught this. These should be within the Trackler module, not
    top-level constants.

--- lib/trackler/doc_file.rb
+++ lib/trackler/doc_file.rb
@@ -1,75 +1,77 @@
-class DocFile
-  DEFAULT_IMAGE_PATH = "/docs/img"
+module Trackler
+  class DocFile
+    DEFAULT_IMAGE_PATH = "/docs/img"
 
-  def self.find(basename:, track_dir:)
-    dir = File.join(track_dir, "docs")
+    def self.find(basename:, track_dir:)
+      dir = File.join(track_dir, "docs")
 
-    [
-      MarkdownFile.new(basename: basename, docs_dir: dir),
-      OrgmodeFile.new(basename: basename, docs_dir: dir),
-    ].detect(&:exist?) || NullFile.new(basename: basename, docs_dir: dir)
+      [
+        MarkdownFile.new(basename: basename, docs_dir: dir),
+        OrgmodeFile.new(basename: basename, docs_dir: dir),
+      ].detect(&:exist?) || NullDocFile.new(basename: basename, docs_dir: dir)
+    end
+
+    attr_reader :basename, :dir
+    def initialize(basename:, docs_dir:)
+      @basename = basename
+      @dir = docs_dir
+    end
+
+    def render(image_path: DEFAULT_IMAGE_PATH)
+      body.gsub(img_src, img_dst(image_path))
+    end
+
+    def name
+      "%s.%s" % [basename, extension]
+    end
+
+    def extension
+      "md"
+    end
+
+    def exist?
+      File.exist?(path)
+    end
+
+    private
+
+    def body
+      File.read(path)
+    end
+
+    def path
+      File.join(dir, name)
+    end
+
+    def img_src
+      Regexp.new("]\\(%s" % DEFAULT_IMAGE_PATH)
+    end
+
+    def img_dst(image_path)
+      "](%s" % image_path.gsub(Regexp.new("/$"), "")
+    end
   end
 
-  attr_reader :basename, :dir
-  def initialize(basename:, docs_dir:)
-    @basename = basename
-    @dir = docs_dir
+  class OrgmodeFile < DocFile
+    def body
+      Orgmode::Parser.new(File.read(path)).to_markdown
+    end
+
+    def extension
+      "org"
+    end
   end
 
-  def render(image_path: DEFAULT_IMAGE_PATH)
-    body.gsub(img_src, img_dst(image_path))
+  class MarkdownFile < DocFile
   end
 
-  def name
-    "%s.%s" % [basename, extension]
-  end
+  class NullDocFile < DocFile
+    def render(image_path:"")
+      ""
+    end
 
-  def extension
-    "md"
-  end
-
-  def exist?
-    File.exist?(path)
-  end
-
-  private
-
-  def body
-    File.read(path)
-  end
-
-  def path
-    File.join(dir, name)
-  end
-
-  def img_src
-    Regexp.new("]\\(%s" % DEFAULT_IMAGE_PATH)
-  end
-
-  def img_dst(image_path)
-    "](%s" % image_path.gsub(Regexp.new("/$"), "")
-  end
-end
-
-class OrgmodeFile < DocFile
-  def body
-    Orgmode::Parser.new(File.read(path)).to_markdown
-  end
-
-  def extension
-    "org"
-  end
-end
-
-class MarkdownFile < DocFile
-end
-
-class NullFile < DocFile
-  def render(image_path:"")
-    ""
-  end
-
-  def exist?
-    false
+    def exist?
+      false
+    end
   end
 end
